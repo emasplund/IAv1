d1a3dfa450ef9cf5d7a61b78592e4bea232abae6
Emily Asplund <asplund.emily@gmail.com>
2023-10-13 22:16:17 -0600

    removed readme

--- src/App.js
+++ src/App.js
@@ -35,22 +35,25 @@ import React from 'react';
 const App = () => {
   return (
     <div>
+
       <header style={{ textAlign: 'center', padding: '20px', backgroundColor: '#333', color: 'white' }}>
         <h1>Student Physical Wellness Resources</h1>
       </header>
+
+      <div style={{ display: 'flex' }}>
       <div style={{ height: '100vh', width: '200px', backgroundColor: '#f0f0f0', padding: '10px', borderRight: '1px solid #ccc' }}>
       <ul style={{ listStyleType: 'none', padding: 0 }}>
         <li>
           <span style={{ marginBottom: '5px', width: '100%', textAlign: 'left' }}>Exercise</span>
           <ul style={{ listStyleType: 'none', padding: 0, paddingLeft: '20px' }}>
             <li>
-              <button style={{ marginBottom: '5px', width: '100%', textAlign: 'left', outline: 'none' }}>Local Resoures</button>
+              <button style={{ marginBottom: '5px', width: '100%', textAlign: 'left', border: 'none' }}>Local Resoures</button>
             </li>
             <li>
-              <button style={{ marginBottom: '5px', width: '100%', textAlign: 'left', outline: 'none' }}>Workout Ideas</button>
+              <button style={{ marginBottom: '5px', width: '100%', textAlign: 'left', border: 'none' }}>Workout Ideas</button>
             </li>
             <li>
-              <button style={{ marginBottom: '5px', width: '100%', textAlign: 'left', outline: 'none' }}>Making a Plan</button>
+              <button style={{ marginBottom: '5px', width: '100%', textAlign: 'left', border: 'none' }}>Making a Plan</button>
             </li>
           </ul>
         </li>
@@ -59,6 +62,12 @@ const App = () => {
         </li>
       </ul>
     </div>
+
+    <div style={{ flex: 1, padding: '20px' }}>
+        <p>Hello, World!</p>
+    </div>
+    </div>
+
     </div>
   );
 };
